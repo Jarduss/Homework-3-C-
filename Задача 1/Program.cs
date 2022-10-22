0cd038467b762c5daec86afa4e15b3c83351535a
danil <jardus@bk.ru>
2022-10-22 20:25:40 +0300

    Homework_3

---
+++ Задача 1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+System.Console.WriteLine();
+System.Console.Write("Введите целое число:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+if (IsPalindrome()) System.Console.WriteLine($"{number} - является палиндромом");
+else System.Console.WriteLine($"{number} - не является палиндромом");
+
+
---
+++ Задача 3.1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+System.Console.Write("Введите число:   ");
+string write = Console.ReadLine();
+int Num = Convert.ToInt32(write);
+int[] GetCube(int Num)
+{
+    int[] cubs = new int[Num];
+    int number = 1;
+    for (int i = 0; i < Num; i++)
+    {
+        cubs[i] = number * number * number;
+        number++;
+    }
+    return cubs;
+}
+void PrintCubs(int[] array)
+{
+    int number = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{number} ^ 3 = ");
+        System.Console.WriteLine(array[i]);
+        number++;
+    }
+}
+int[] CubeTable = GetCube(Num);
+PrintCubs(CubeTable);
